8066743ced09c21d44441417bca18fef3b1d6371
COFFEEISLIFE\melan <melanieroyplommer@gmail.com>
2018-05-08 10:40:35 -0300

    Revert "added rolling in source"
    This reverts commit 45f3c15feefe121d74f8fecb7fc59dddfb0156b7.

--- Dice/Dice/Source.cpp
+++ Dice/Dice/Source.cpp
@@ -2,23 +2,10 @@
 //
 //
 #include <iostream>
-#include "PairOfDice.h"
 
 int main() {
 
-	PairOfDice dice;
-
-	for (int i = 0; i < 100; i++) {
-
-
-		std::cout << dice.roll() << " ";
-		if (dice.isGoToJail()) {
-		
-			std::cout << "\n\n***GO TO JAIL**\n\n";
-		}
-	}
-
-	//std::cout << "Hello World\n";
+	std::cout << "Hello World\n";
 
 	getchar();
 
