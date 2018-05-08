45f3c15feefe121d74f8fecb7fc59dddfb0156b7
COFFEEISLIFE\melan <melanieroyplommer@gmail.com>
2018-05-08 10:39:46 -0300

    added rolling in source

--- Dice/Dice/Source.cpp
+++ Dice/Dice/Source.cpp
@@ -2,10 +2,23 @@
 //
 //
 #include <iostream>
+#include "PairOfDice.h"
 
 int main() {
 
-	std::cout << "Hello World\n";
+	PairOfDice dice;
+
+	for (int i = 0; i < 100; i++) {
+
+
+		std::cout << dice.roll() << " ";
+		if (dice.isGoToJail()) {
+		
+			std::cout << "\n\n***GO TO JAIL**\n\n";
+		}
+	}
+
+	//std::cout << "Hello World\n";
 
 	getchar();
 
